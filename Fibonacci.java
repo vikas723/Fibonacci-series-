fb9ba6cfe62960cc01f2bad6f2212d49b9397375
vikas723 <81210333+vikas723@users.noreply.github.com>
2021-08-29 13:20:04 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,12 @@
+public class Fibonacci{
+    public static void main(String args[]) {
+      int n=11,firstTerm=0,secondTerm=1;
+      System.out.println("Fibonacci Series till" + n + "terms:");
+      for(int i=1;i<=n;i++) {
+          System.out.print(firstTerm+",");
+          int nextTerm = firstTerm+secondTerm;
+          firstTerm = secondTerm;
+          secondTerm = nextTerm;
+      }
+    }
+}
